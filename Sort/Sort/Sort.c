6fc90bcb05c5e0332388aa3ba86cf7cc39b30a84
Coke <1760198676@qq.com>
2025-02-27 16:33:29 +0800

    1

--- Sort/Sort/Sort.c
+++ Sort/Sort/Sort.c
@@ -37,20 +37,89 @@ void BubbleSort(int* arr, size_t size) {
 
 
 //选择排序
-void SelectSort(int* arr, size_t size) {
+void SelectSort1(int* arr, size_t size) {
+	//外层[0,n-1]区间的值需要作比较 有size个元素，则比较size - 1趟
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		//记录当前轮次最小值的索引
+		size_t mini = i;
+		//每一轮前面的元素都要和后面所有元素比较，比到最后一个
+		for (size_t j = i + 1; j < size; j++)
+		{
+			if (arr[i] > arr[j])
+				mini = j;
+		}
+		if (mini != i) 
+			Swap(&arr[i], &arr[mini]);
+	}
+}
+//选择排序
+void SelectSort2(int* arr, size_t size) {
 	size_t left = 0, right = size - 1;
 
-	while (left <= right) {
-		size_t mini = left, maxi = right;
-		for (size_t i = 0; i < size - 1; i++)
+	while (left < right) {
+		//一轮下俩 最大值和最小值都能找到 分别放到最左边和最右边
+		size_t mini = left, maxi = left;
+		//假设第一个值是最大值最小值，只需要从第二个值开始遍历 找新的最大值最小值
+		for (size_t i = left + 1; i <= right; i++)
 		{
-			if (arr[i] < arr[mini])	mini = i;
-			if (arr[i] > arr[maxi]) maxi = i;
+			//遇到更小的值/更大的值就更新索引
+			if (arr[i] < arr[mini])	
+				mini = i;
+			if (arr[i] > arr[maxi]) 
+				maxi = i;
 		}
+		//将最小的值放到左侧 最大的值放到右侧
 		Swap(&arr[left], &arr[mini]);
 		if (maxi == left) maxi = mini;
 		Swap(&arr[right], &arr[maxi]);
+		//开始新一轮查找
 		left++;
 		right--;
 	}
+}
+
+//插入排序
+void InsertSort(int* arr, size_t size) {
+
+	for (size_t i = 0; i < size - 1; i++)
+	{
+		size_t end = i;
+		int base = arr[end + 1];
+		while (end >= 0) {
+			if (arr[end] > base) {
+				arr[end + 1] = arr[end];
+				end--;
+			}
+			else
+				break;
+		}
+		arr[end + 1] = base;
+	}
+
+}
+
+//希尔排序
+void ShellSort(int* arr, size_t size) {
+	int gap = size;
+	while (gap > 1){
+		gap = gap / 3 + 1;
+		for (size_t i = 0; i < size - gap; i++)
+		{
+			//这里的end必须是int类型 如果end - gap成为负数 用size_t类型会越界访问
+			int end = i;
+			int base = arr[end + gap];
+			while (end >= 0) {
+				if (arr[end] > base) {
+					arr[end + gap] = arr[end];
+					end -= gap;
+				}
+				else
+					break;
+			}
+			arr[end + gap] = base;
+		}
+
+	}
+	
 }
--- Sort/Sort/Test.c
+++ Sort/Sort/Test.c
@@ -1,21 +1,36 @@
 #include "Sort.h"
 
 void TestBubbleSort() {
-	int arr[] = { 2,4,1,3,6,7,9,8,5 };
+	int arr[] = { 2,4,1,3,6,7,9,8,5,12,65,45,33,14};
 	PrintArray(arr, sizeof(arr) / sizeof(int));
 	BubbleSort(arr, sizeof(arr) / sizeof(int));
 	PrintArray(arr, sizeof(arr) / sizeof(int));
 }
 
 void TestSelectSort() {
-	int arr[] = { 2,4,1,3,6,7,9,8,5 };
+	int arr[] = { 2,4,1,3,6,7,9,11,8,5,0};
 	PrintArray(arr, sizeof(arr) / sizeof(int));
-	SelectSort(arr, sizeof(arr) / sizeof(int));
+	SelectSort1(arr, sizeof(arr) / sizeof(int));
+	PrintArray(arr, sizeof(arr) / sizeof(int));
+}
+
+void TestInsertSort() {
+	int arr[] = { 2,4,1,3,6,7,9,8,5,0 };
+	PrintArray(arr, sizeof(arr) / sizeof(int));
+	InsertSort(arr, sizeof(arr) / sizeof(int));
+	PrintArray(arr, sizeof(arr) / sizeof(int));
+}
+void TestShellSort() {
+	int arr[] = { 2,4,1,3,6,7,9,8,5,0,41,14,36,12,33 };
+	PrintArray(arr, sizeof(arr) / sizeof(int));
+	ShellSort(arr, sizeof(arr) / sizeof(int));
 	PrintArray(arr, sizeof(arr) / sizeof(int));
 }
 
 int main() {
 	/*TestBubbleSort();*/
-	TestSelectSort();
+	/*TestSelectSort();*/
+	/*TestInsertSort();*/
+	TestShellSort();
 	return 0;
 }
